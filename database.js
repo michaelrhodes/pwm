9ee9935517d11ce7809016f75d36f8472fd342cd
Michael Rhodes <spam@michaelrhod.es>
2014-03-19 19:02:45 +1000

    Added support for semver versioning.

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ var nedb = require('nedb')
 var mkdir = require('fs').mkdirSync
 var join = require('path').join
 var home = require('home-dir')()
+var semver = require('semver')
 var Fuse = require('fuse.js')
 
 var directory = join(home, '.pwm')
@@ -30,7 +31,21 @@ request('http://modules.processwire.com/export-json?apikey=pw223&limit=100')
 
 module.exports = {
   find: function(module, callback) {
-    db.findOne({ class_name: module })
+    var parts = module.split('@')
+    var name = parts[0]
+    var version = parts[1]
+
+    var query = {
+      class_name: name
+    }
+
+    if (version) {
+      query.$where = function() {
+        return semver.satisfies(this.module_version, version)
+      }
+    }
+
+    db.findOne(query)
       .exec(function(error, info) {
         if (error) {
           callback(error)
@@ -39,7 +54,7 @@ module.exports = {
 
         if (!info) {
           return callback(new Error(
-            'Could not find ' + module
+            'Could not find ' + name + (version ? '@' + version : '')
           ))
         }
 
